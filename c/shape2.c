62c34e012f4ffca7956dbb18ef493be40f0690ef
lunarinaa <aydan.akbarova@gmail.com>
2023-10-13 10:31:08 -0700

    triangle

---
+++ c/shape2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * return: 0 (Succsess)
+ */
+int main()
+{
+printf("     /|\n");
+printf("    / |\n");
+printf("   /  |\n");
+printf("  /___|\n");
+return 0;
+}
