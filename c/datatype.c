9b41997261e769900f88b866d55708ee020f16c6
lunarinaa <aydan.akbarova@gmail.com>
2023-10-15 23:34:09 -0700

    data types

---
+++ c/datatype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/* int whole number */
+int age = 40; 
+/* decimal */
+  double gpa = 40.0;
+/* character */
+char grade = 'F';
+/*more than one character with []*/
+char phrase [] = "This is a phrase" ;
+  
+    return 0;
+}
