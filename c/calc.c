53709adc6b2233ca6d64123f35c5f6eb9db947ae
lunarinaa <aydan.akbarova@gmail.com>
2023-10-16 22:11:38 -0700

    calc

---
+++ c/calc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    printf("Enter first number: \n");
+    scanf("%d", &num1);
+    printf("Enter second number: \n");
+    scanf("%d", &num2);
+    printf("Answer: %d \n", num1 + num2);
+    return 0;
+
+}
